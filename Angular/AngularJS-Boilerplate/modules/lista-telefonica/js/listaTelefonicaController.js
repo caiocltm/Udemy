7a7c287341a454890cbaaf764c8f4c372bb837a5
caiocltm <caiocltm@github.com>
2019-01-12 20:35:59 -0200

    Angular

--- Angular/AngularJS-Boilerplate/modules/lista-telefonica/js/listaTelefonicaController.js
+++ Angular/AngularJS-Boilerplate/modules/lista-telefonica/js/listaTelefonicaController.js
@@ -1,34 +1,40 @@
 angular.module('lista-telefonica', [])
 
-.controller('listaTelefonicaController', function($scope){
-    
-    $scope.appName = "Lista Telefônica";
-    $scope.titleContatos = "Contatos";
-    $scope.titleForm = "Cadastro de Contatos";
-    $scope.titleAddButton = "Adicionar Contato";
-    $scope.titleDelButton = "Excluir Contato";
-    $scope.contatos = [
-        {nome: "Caio César", telefone: "991199683"},
-        {nome: "Vitor Calazans", telefone: "9911232090"},
-        {nome: "Marli Lopes", telefone: "991146326"}
-    ];
-    $scope.operadoras = [
-        {nome: "Oi", codigo: 14, categoria: "Móvel"},
-        {nome: "Vivo", codigo: 15, categoria: "Móvel"},
-        {nome: "Tim", codigo: 41, categoria: "Móvel"},
-        {nome: "NET", codigo: 27, categoria: "Fixo"},
-        {nome: "GVT", codigo: 25, categoria: "Fixo"},
-        {nome: "Embratel", codigo: 21, categoria: "Fixo"}
-    ];
-    
-    $scope.adicionarContato = function(contato){
-        $scope.contatos.push(angular.copy(contato));
-        delete $scope.contato;
-    };
+    .controller('listaTelefonicaController', function ($scope) {
 
-    $scope.excluirContato = function(contatos){
-        $scope.contatos = contatos.filter(function(contato){
-            if(!contato.selecionado) return contato;
-        });
-    };  
-})
+        $scope.appName = "Lista Telefônica";
+        $scope.titleContatos = "Lista de Contatos";
+        $scope.titleForm = "Cadastro de Contatos";
+        $scope.titleAddButton = "Adicionar Contato";
+        $scope.titleDelButton = "Excluir Contato";
+        $scope.contatos = [
+            { nome: "Caio César", telefone: "991199683", cor: "red" },
+            { nome: "Vitor Calazans", telefone: "9911232090", cor: "blue" },
+            { nome: "Marli Lopes", telefone: "991146326", cor: "yellow" }
+        ];
+        $scope.operadoras = [
+            { nome: "Oi", codigo: 14, categoria: "Móvel" },
+            { nome: "Vivo", codigo: 15, categoria: "Móvel" },
+            { nome: "Tim", codigo: 41, categoria: "Móvel" },
+            { nome: "NET", codigo: 27, categoria: "Fixo" },
+            { nome: "GVT", codigo: 25, categoria: "Fixo" },
+            { nome: "Embratel", codigo: 21, categoria: "Fixo"}
+        ];
+
+        $scope.adicionarContato = function (contato) {
+            $scope.contatos.push(angular.copy(contato));
+            delete $scope.contato;
+        };
+
+        $scope.excluirContato = function (contatos) {
+            $scope.contatos = contatos.filter(function (contato) {
+                if (!contato.selecionado) return contato;
+            });
+        };
+
+        $scope.isContatoSelecionado = function (contatos) {
+            return $scope.contatos.some(function (contato) {
+                return contato.selecionado;
+            });
+        };
+    })
