f39fefe80eb2354ab096ab23824c411e258b29cd
caio.menezes <caio.menezes@github.com>
2019-11-02 20:03:48 -0300

    [NodeJS] Seção 11

--- JavaScript/NodeJS/NodeJS_Complete_Guide/Seção_11/controllers/admin.js
+++ JavaScript/NodeJS/NodeJS_Complete_Guide/Seção_11/controllers/admin.js
@@ -13,55 +13,63 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
+    description: description,
   })
-  .then(result => console.log('MySQL Success: ', result))
+  .then(result => {
+    console.log('Product created with success!');
+    res.redirect('/admin/products');
+  })
   .catch(err => console.log('MySQL Error: ', err));
 };
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
   if(!editMode) return res.redirect('/');
-  const prodId = req.params.productId;
-  Product.findByPk(prodId)
-  .then(([rows]) => {
-    if(!rows) res.redirect('/');
+  const prodId = parseInt(req.params.productId);
+  req.user.getProducts({where: {id: prodId}})
+  .then(products => {
+    !products && res.redirect('/');
+    const product = products[0];
     res.render('admin/edit-product', {
       pageTitle: 'Edit Product',
       path: '/admin/edit-product',
       editing: editMode,
-      product: rows
+      product: product
     });
   })
   .catch(err => console.log('MySQL Error: ', err));
 };
 
 exports.postEditProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+  const prodId = parseInt(req.body.productId);
   const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
+  const updatedPrice = parseFloat(req.body.price);
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId, 
-    updatedTitle, 
-    updatedImageUrl, 
-    updatedDesc, 
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  })
+  .then(result => {
+    console.log('Updated Product!')
+    res.redirect('/admin/products');
+  })
+  .catch(err => console.log('MySQL Error: ', err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows]) => {
+  req.user.getProducts()
+  .then(products => {
     res.render('admin/products', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
@@ -70,9 +78,13 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.postDeleteProduct = (req, res, next) => {
-    const prodId = req.body.productId;
-    Product.deleteById(prodId)
-    .then(result => console.log('Product deleted with success!'))
+    const prodId = parseInt(req.body.productId);
+    
+    Product.findByPk(prodId)
+    .then(product => product.destroy())
+    .then(result => {
+      console.log('Product deleted with success!');
+      res.redirect('/admin/products');
+    })
     .catch(err => console.log('MySQL Error: ', err));
-    res.redirect('/admin/products');
 };
--- JavaScript/NodeJS/NodeJS_Complete_Guide/Seção_11/controllers/shop.js
+++ JavaScript/NodeJS/NodeJS_Complete_Guide/Seção_11/controllers/shop.js
@@ -1,25 +1,25 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll()
+  .then(products => {
     res.render('shop/product-list', {
-      prods: rows,
+      prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  })
-  .catch(err => console.log('MySQL Error: ', err));
+  }).catch(err => {
+    console.log('MySQL Error: ', err);
+  });
 };
 
 exports.getProduct = (req, res, next) => {
-  const prodId = req.params.productId;
+  const prodId = parseInt(req.params.productId);
   Product.findByPk(prodId)
-  .then(([rows]) => {
+  .then(product => {
     res.render('shop/product-detail', {
-      product: rows[0], 
-      pageTitle: rows[0].title,
+      product: product, 
+      pageTitle: product.title,
       path: "/products"
     });
   })
@@ -27,70 +27,109 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll()
-  .then(([rows, fieldData]) => {
+  Product.findAll().then(products => {
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
-  })
-  .catch(err => console.log('MySQL Error: ', err));
-};
-
-exports.getCart = (req, res, next) => {
-  Cart.getCart(cart => {
-    Product.fetchAll()
-    .then(([rows, fieldData]) => {
-      const cartProducts = [];
-      for(product of rows) {
-        const cartProductData = cart.products.find(prod => prod.id === product.id);
-        if(cartProductData) {
-          cartProducts.push({
-              productData: product, 
-              qty: cartProductData.qty
-            }
-          );
-        }
-      }
-      res.render('shop/cart', {
-        path: '/cart',
-        pageTitle: 'Your Cart',
-        products: cartProducts
-      });
-    })
-    .catch(err => console.log('MySQL Error: ', err));
+  }).catch(err => {
+    console.log('MySQL Error: ', err);
   });
 };
 
+exports.getCart = (req, res, next) => {
+  req.user.getCart()
+  .then(cart => {
+    return cart.getProducts()
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+    })
+    .catch(err => console.log('Fetch Products Error: ', err));
+  }).catch(err => console.log('Fetch Cart Error: ', err));
+};
+
 exports.postCart = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findByPk(prodId)
-  .then(([rows]) => Cart.addProduct(prodId, rows.price))
-  .catch(err => console.log('MySQL Error: ', err));
-  res.redirect('/cart');
+  const prodId = parseInt(req.body.productId);
+  let fetchedCart;
+  let newQuantity = 1;
+
+  req.user.getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts({where: {id: prodId}});
+  })
+  .then(products => {
+    let product;
+    if(products.length > 0) product = products[0];
+    if(product) {
+      const oldQuantity = product.cartItem.qty;
+      newQuantity = oldQuantity + 1;
+      return product;
+    }
+    return Product.findByPk(prodId)
+    .then(product => {
+      return fetchedCart.addProduct(product, {through: {qty: newQuantity}});
+    })
+    .catch(err => console.log('Fetch Product Error: ', err));
+  })
+  .then(product => {
+    return fetchedCart.addProduct(product, {
+      through: {qty: newQuantity}
+    });
+  })
+  .then(_ => res.redirect('/cart'))
+  .catch(err => console.log('Fetch Cart Error: ', err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findByPk(prodId)
-  .then(([rows]) => {
-    Cart.deleteProduct(prodId, rows.price);
-    res.redirect('/cart');
+  req.user.getCart()
+  .then(cart => {
+    return cart.getProducts({where: {id: prodId}});
   })
-  .catch(err => console.log('MySQL Error: ', err));
-};
+  .then(products => {
+    const product = products[0];
+    product.cartItem.destroy();
+  })
+  .then(result => res.redirect('/cart'))
+  .catch(err => console.log('Fetch Cart Error: ', err));
+}; 
 
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
-  });
+  req.user.getOrders({include: ['products']})
+  .then(orders => {
+    res.render('shop/orders', {
+      path: '/orders',
+      pageTitle: 'Your Orders',
+      orders: orders
+    });
+  })
+  .catch(err => console.log('Fetch Cart Error: ', err));
 };
 
-exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
-  });
+exports.postOrder = (req, res, next) => {
+  let fetchedCart;
+  req.user.getCart()
+  .then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts();
+  })
+  .then(products => {
+    return req.user.createOrder()
+    .then(order => {
+      return order.addProducts(products.map(product => {
+        product.orderItem = {qty: product.cartItem.qty};
+        return product;
+      }));
+    })
+    .catch(err => console.log('Fetch Orders Error: ', err))
+  })
+  .then(result => fetchedCart.setProducts(null))
+  .then(result => res.redirect('/orders'))
+  .catch(err => console.log('Fetch Cart Error: ', err));
 };
